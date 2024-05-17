d58e79bccfd04bfceae4fc467787b8e0b18f8f55
siam-sharif-ami <siam.sharif@brainstation-23.com>
2024-05-17 14:42:11 +0600

    Added Navigation bottom bar

---
+++ Cinemo/Preview Content/NavigationBottom.swift
@@ -0,0 +1,18 @@
+//
+//  NavigationBottom.swift
+//  Cinemo
+//
+//  Created by BS00484 on 17/5/24.
+//
+
+import SwiftUI
+
+struct NavigationBottom: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    NavigationBottom()
+}
