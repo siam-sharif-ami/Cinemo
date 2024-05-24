cc77c0f35b8c5dd76c24c65ca7b2b79076b64e04
siam-sharif-ami <siam.sharif@brainstation-23.com>
2024-05-24 20:06:51 +0600

    add search Functionality

---
+++ Cinemo/ViewModel/SearchViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  SearchViewModel.swift
+//  Cinemo
+//
+//  Created by BS00484 on 24/5/24.
+//
+
+import Foundation
---
+++ Cinemo/Views/SearchTabView.swift
@@ -0,0 +1,18 @@
+//
+//  SearchTabView.swift
+//  Cinemo
+//
+//  Created by BS00484 on 24/5/24.
+//
+
+import SwiftUI
+
+struct SearchTabView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SearchTabView()
+}
